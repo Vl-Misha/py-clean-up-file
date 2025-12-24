9c206a36312cd9575c101e99122b5a3edd42694f
Vlasov Misha <vlasovmisha.060@gmail.com>
2025-12-24 21:26:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 import os
 
-
 class CleanUpFile:
-    def __init__(self, file_name: str):
-        self.file_name = file_name
+    def __init__(self, filename: str):
+        self.filename = filename  # зберігаємо назву файлу
 
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type: None, exc_value: None, traceback: None):
-        if os.path.exists("file.txt"):
-            os.remove("file.txt")
+    def __exit__(self, exc_type, exc_value, traceback):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
