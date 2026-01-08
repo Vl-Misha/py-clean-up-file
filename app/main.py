c536f69ba7415ccc2f43bc452c0ed73c1f8321ed
Vlasov Misha <vlasovmisha.060@gmail.com>
2026-01-08 22:05:02 +0200

    olution

--- app/main.py
+++ app/main.py
@@ -5,9 +5,15 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> CleanUpFile:
+    def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if os.path.exists(self.filename):
+        try:
             os.remove(self.filename)
+
+        except FileNotFoundError:
+            pass
+
+        except PermissionError:
+            pass
