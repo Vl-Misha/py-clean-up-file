3ccab29852fe670403fcb1f35939673a43aef041
Vlasov Misha <vlasovmisha.060@gmail.com>
2026-01-08 21:39:32 +0200

    olution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import os
 
-class CleanUpFile:
-    def __init__(self, filename: str):
-        self.filename = filename  # зберігаємо назву файлу
 
-    def __enter__(self):
+class CleanUpFile:
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> CleanUpFile:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
